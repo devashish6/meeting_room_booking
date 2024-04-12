0c7e9228a9152839e28fdd63d82ef3a99fd8dcda
Devashish <devashishmurthyk@gmail.com>
2024-04-12 16:37:22 +0530

    firebase dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,6 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.hiltAndroid) apply false
+    alias(libs.plugins.firebase) apply false
+    alias(libs.plugins.androidLibrary) apply false
 }
