053206645813785e75a452c0e2ae9c4329d86728
Devashish <devashishmurthyk@gmail.com>
2024-04-12 16:37:40 +0530

    calendar view

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.booking.slots"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
--- app/src/main/java/com/booking/slots/MainActivity.kt
+++ app/src/main/java/com/booking/slots/MainActivity.kt
@@ -1,30 +1,156 @@
 package com.booking.slots
 
 import android.os.Bundle
+import android.view.ContextThemeWrapper
+import android.widget.CalendarView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import com.booking.slots.ui.theme.SlotsTheme
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SlotsTheme {
+                DatePicker(onDateSelected = {}) {
+
+
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun DatePicker(onDateSelected: (LocalDate) -> Unit, onDismissRequest: () -> Unit) {
+    val selDate = remember { mutableStateOf(LocalDate.now()) }
+
+    Dialog(onDismissRequest = { onDismissRequest() }, properties = DialogProperties()) {
+        Column(
+            modifier = Modifier
+                .wrapContentSize()
+                .background(
+                    color = MaterialTheme.colorScheme.surface,
+                    shape = RoundedCornerShape(size = 16.dp)
+                )
+        ) {
+            Column(
+                Modifier
+                    .defaultMinSize(minHeight = 72.dp)
+                    .fillMaxWidth()
+                    .background(
+                        color = MaterialTheme.colorScheme.primary,
+                        shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)
+                    )
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = "Select date".uppercase(Locale.ENGLISH),
+                    style = MaterialTheme.typography.labelLarge,
+                    color = MaterialTheme.colorScheme.onPrimary
+                )
+
+                Spacer(modifier = Modifier.size(24.dp))
+
+                Text(
+                    text = selDate.value.format(DateTimeFormatter.ofPattern("dd-MMM-YYYY")),
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = MaterialTheme.colorScheme.onPrimary
+                )
+
+                Spacer(modifier = Modifier.size(16.dp))
+            }
+
+            CustomCalendarView(onDateSelected = {
+                selDate.value = it
+            })
+
+            Spacer(modifier = Modifier.size(8.dp))
+
+            Row(
+                modifier = Modifier
+                    .align(Alignment.End)
+                    .padding(bottom = 16.dp, end = 16.dp)
+            ) {
+                TextButton(
+                    onClick = onDismissRequest
+                ) {
+                    Text(
+                        text = "Cancel",
+                        style = MaterialTheme.typography.labelMedium,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                }
+
+                TextButton(
+                    onClick = {
+                        onDateSelected(selDate.value)
+                        onDismissRequest()
+                    }
+                ) {
+                    Text(
+                        text = "OK",
+                        style = MaterialTheme.typography.labelMedium,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                }
 
             }
         }
     }
 }
 
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//
-//}
+@Composable
+fun CustomCalendarView(onDateSelected: (LocalDate) -> Unit) {
+    AndroidView(
+        modifier = Modifier.wrapContentSize(),
+        factory = {
+            CalendarView(ContextThemeWrapper(it, R.style.Theme_Slots))
+        },
+        update = { view ->
+            view.setOnDateChangeListener { _, year, month, dayOfMonth ->
+                onDateSelected(
+                    LocalDate
+                        .now()
+                        .withMonth(month + 1)
+                        .withYear(year)
+                        .withDayOfMonth(dayOfMonth)
+                )
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DatePickerPreview() {
+    DatePicker(onDateSelected = {}) {
+    }
+}
